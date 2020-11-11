d7e8deabac31182dcf9734190c9b212a40e95085
natalia3636 <nataliapaulino36@gmail.com>
2020-11-11 10:05:27 -0300

    aula11_11

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
 url={dadosIniciais.categorias[0].videos[0].url}
 videoDescription={"Segundo a norma ISO IEC 9241-11 criada em 1998 Usabilidade é: a capacidade de um produto ser usado por usuários específicos para atingir objetivos específicos com eficácia, eficiência e satisfação em um contexto específico de uso."}
 />
-            <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]}/>            />
+            <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]}/>            
             <Carousel category={dadosIniciais.categorias[1]}/>
             <Carousel category={dadosIniciais.categorias[2]}/>
             <Carousel category={dadosIniciais.categorias[3]}/>
