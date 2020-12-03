c7758bb9f9ca7a17f13421e4224a2c2c10c26155
natalia3636 <nataliapaulino36@gmail.com>
2020-12-02 22:48:07 -0300

    videos-categorias

--- src/pages/Categorias/index.js
+++ src/pages/Categorias/index.js
@@ -1,10 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import ModalDialog from "../../components/Modal";
 import { useForm } from 'react-hook-form';
+import { Link, useHistory } from 'react-router-dom';
 import "./index.css"
+
 function CadastroCategoria() {
 const { register, handleSubmit, errors } = useForm();
+const history = useHistory();
+
+const [open, setOpen] = React.useState(false);
+const handleClickOpen = () => {setOpen(true);};
+const handleNo = () => {
+setOpen(false);
+history.push('/ ');
+};
+const handleYes = () => {
+setOpen(false);
+};
+
 
 function onSubmit(dados) {
     console.log("Dados:", dados);
@@ -20,17 +35,21 @@ function onSubmit(dados) {
     })
     .catch((error) => console.error("Error:", error))
     .then((response) => console.log("Success:", JSON.stringify(response)))
+    .then(handleClickOpen())
     
     console.log(dados);
     }
     
 return (
-<div className="corpo">
+
+<div>
 <Header />
-<h1> Cadastro Categoria </h1>
+<div className="corpo">
+{/* <form onSubmit={handleSubmit(onSubmit)} className="form-group">  */}
 
-<form onSubmit={handleSubmit(onSubmit)} className="form-group">
-
+<form className="container"id="video-form"onSubmit={handleSubmit(onSubmit)}
+>
+<h1> Cadastro de Categorias</h1>
 <input type="text" className="form-control" placeholder="Título" name="titulo" ref={register({required: true})} />
 
 <br />
@@ -49,8 +68,22 @@ return (
 <input type="submit" className="btn btn-primary btn-lg" value="Salvar"/>
 
 </form>
+<Link to="/">
+    Ir para home
+</Link>
+</div>
+    <ModalDialog
+    open = {open}
+    handleClose = {handleNo}
+    titulo = 'Cadastrado com sucesso'
+    texto = 'Deseja fazer outro cadastro?'
+    handleSim = {handleYes}
+    handleNao = {handleNo}
+    />
+    
+
 <Footer />
 </div>
-)
-};
+);
+}
 export default CadastroCategoria;
